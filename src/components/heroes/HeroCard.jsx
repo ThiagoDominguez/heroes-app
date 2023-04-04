6aaf47b9b9313a44f6fe6dd0a137cd1178699e93
ThiagoDominguez <credgeyt@gmail.com>
2023-04-04 16:21:14 -0300

    fixing build version

--- src/components/heroes/HeroCard.jsx
+++ src/components/heroes/HeroCard.jsx
@@ -14,7 +14,8 @@ export const HeroCard = ({
       <div className="row g-0">
         <div className="col-md-4">
           <img
-            src={`/src/components/heroes/assets/heroes/${id}.jpg`}
+            // src={`/src/components/heroes/assets/heroes/${id}.jpg`}
+            src={`/public/assets/heroes/${id}.jpg`}
             className="img-card-top  w-100"
             alt={superhero}
           />
